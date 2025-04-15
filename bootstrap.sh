1260ff20de69c35d85da5c60bfaf80cf43530122
Greg Troszak <greg@gregtroszak.me>
2025-04-15 12:04:39 -0400

    Make bootstrap posix compliant.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -euo pipefail
+set -eu
 
 REPO_URL="https://github.com/gdtroszak/dotfiles.git"
 CLONE_DIR="${HOME}/.dotfiles"
@@ -15,7 +15,7 @@ fi
 
 . "$CONFIG_ZSH_PATH/functions.zsh"
 
-if [[ "$(uname)" != "Darwin" ]]; then
+if [ "$(uname)" != "Darwin" ]; then
   echo "Error: This bootstrap script currently only supports macOS." >&2
   exit 1
 fi
@@ -25,9 +25,9 @@ if ! is_installed brew ; then
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   # Make Homebrew available in this shell session
-  if [[ -x /opt/homebrew/bin/brew ]]; then
+  if [ -x /opt/homebrew/bin/brew ]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
-  elif [[ -x /usr/local/bin/brew ]]; then
+  elif [ -x /usr/local/bin/brew ]; then
     eval "$(/usr/local/bin/brew shellenv)"
   else
     echo "Error: brew was installed but not found in expected locations." >&2
@@ -54,15 +54,11 @@ XDG_STATE_HOME=$HOME/.local/state
 ZDOTDIR=$XDG_CONFIG_HOME/zsh
 
 # Create XDG directories (and some others)
-mkdir -p $XDG_CACHE_HOME
-mkdir -p $XDG_DATA_HOME
-mkdir -p $XDG_STATE_HOME
-# This also makes $XDG_CONFIG_HOME.
-mkdir -p $ZDOTDIR
+mkdir -p "$XDG_CACHE_HOME" "$XDG_DATA_HOME" "$XDG_STATE_HOME" "$ZDOTDIR"
 
 stow zsh
 stow tmux
 
 echo "Dotfiles stowed successfully."
 
-exec zsh -i
+exec zsh -l
