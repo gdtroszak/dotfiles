702910e3972e167a3578fc15aa61f7876815913a
Greg Troszak <greg@gregtroszak.me>
2025-04-17 10:05:30 -0400

    Use deno to format css.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -778,6 +778,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         markdown = { 'deno_fmt' },
+        css = { 'deno_fmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
