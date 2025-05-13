d2a873a237351caec6d2a3ea750562ebf1b50617
Greg Troszak <greg@gregtroszak.me>
2025-05-13 12:27:33 -0400

    Make w3m XDG compliant.

--- aerc/.config/zsh/.zshrc.d/aerc.zsh
+++ aerc/.config/zsh/.zshrc.d/aerc.zsh
@@ -4,6 +4,7 @@ if ! is_installed aerc; then
   brew install aerc
 fi
 
+export W3M_DIR="$XDG_STATE_HOME/w3m"
 # Ensure w3m is installed
 if ! is_installed w3m; then
   echo "[aerc.zsh] w3m not found, installing..."
