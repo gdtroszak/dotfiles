8b26416b65f5876d133a86377bd5830218c39aa1
Greg Troszak <greg@gregtroszak.me>
2025-07-23 06:53:35 -0400

    configure docker to use colima.

--- docker/.config/zsh/.zshrc.d/docker.zsh
+++ docker/.config/zsh/.zshrc.d/docker.zsh
@@ -1,7 +1,13 @@
 export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
 
-# Ensure desktop is installed
-if ! brew list --cask docker-desktop >/dev/null 2>&1; then
-  echo "[docker.zsh] docker-desktop not found, installing..."
-  brew install docker-desktop
+# Ensure colima is installed
+if ! is_installed colima; then
+  echo "[docker.zsh] colima not found, installing..."
+  brew install colima
+fi
+
+# Ensure docker is installed
+if ! is_installed docker; then
+  echo "[docker.zsh] docker not found, installing..."
+  brew install docker
 fi
